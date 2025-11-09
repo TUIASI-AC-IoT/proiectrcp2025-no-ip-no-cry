fb732323492f9e466104c0615aa1156b32daab6a
Dascalu Felicia <ioana-felicia.dascalu@student.tuiasi.ro>
2025-11-09 05:52:30 +0200

    first connection between agent and manager with UDP

--- agent.py
+++ agent.py
@@ -1,24 +1,66 @@
+#importarea modulelor necesare
 import socket
+import sys
 
-agent_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-agent_socket.connect(('127.0.0.1', 12345))
+#verificarea argumentelor din linia de comanda
+if len(sys.argv) < 2:
+    print("EROARE: Compilare incorecta! (ex: python agent.py 12345)")
+    sys.exit(1)
 
-payload = 'Hey Server'
+try:
+    AGENT_PORT = int(sys.argv[1])
+except ValueError:
+    print("EROARE: Numar invalid. (ex: python agent.py 12345)")
+    sys.exit(1)
+
+#configurarea adresei agentului
+AGENT_ADDR = ('127.0.0.1', AGENT_PORT)
+
+#crearea socket-ului UDP pentru agent
+agent_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+#setarea timpului de expirare pentru recvfrom
+agent_socket.settimeout(5.0)  #timeout de 5 secunde  
+
+#legarea socket-ului la adresa agentului
+try:
+    agent_socket.bind(('0.0.0.0', AGENT_PORT))
+except OSError as e:
+    print(f"EROARE: Nu pot lega socket-ul pe portul {AGENT_PORT}.\nMotiv: {e}")
+    sys.exit(1)
+
+
+print(f"Agentul asculta pe UDP {AGENT_ADDR}...")
 
 try:
     while True:
-        agent_socket.send(payload.encode('utf-8'))
-        data = agent_socket.recv(1024)
-        print(str(data))
+        try:
+            #primirea cererii de la manager
+            data, manager_addr = agent_socket.recvfrom(1024)
+            
+            #afisarea cererii primite
+            request_msg = data.decode()
+            print(f"\n[RECV] Cerere de la Manager ({manager_addr}): {request_msg}")
 
-        more = input('Wnat to send more data? (y/n): ')
-        if more.lower() == 'y':
-            payload = input('Enter data to send: ')
-        else:
-            break
+            #pregatirea raspunsului in functie de cerere
+            if "Agent 1" in request_msg:
+                response_data = b"Response: System Description - Port 12345 OK"
+            elif "Agent 2" in request_msg:
+                response_data = b"Response: System UpTime - Port 12346 OK"
+            else:
+                response_data = b"Response: Unknown Request"
 
+            #trimiterea raspunsului catre manager
+            agent_socket.sendto(response_data, manager_addr)
+            print(f"[SEND] Răspuns trimis către Manager.")
+            
+        except socket.timeout:
+            #daca nu s-au primit cereri in timpul de asteptare
+            pass
+
+#exceptia pentru oprirea agentului cu Ctrl+C
 except KeyboardInterrupt:
-    print("Exit by user")
-
+    print("\nAgentul oprit.")
+    
+#inchiderea socket-ului agentului
 agent_socket.close()
-
--- manager.py
+++ manager.py
@@ -1,25 +1,69 @@
+#importarea modulelor necesare
 import socket
+import select
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('127.0.0.1', 12345))
+#configurarea adreselor agentilor pe localhost
+AGENT_1_ADDR = ('127.0.0.1', 12345)
+AGENT_2_ADDR = ('127.0.0.1', 12346)
 
-server_socket.listen(5)
+#crearea socket-ului UDP pentru manager
+manager_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-while True:
-    print("Waiting for a connection...")
-    client_socket, addr = server_socket.accept()
-    print(f"Connection from %s has been established!" % str(addr))
+#legarea socket-ului la o adresa locala
+manager_socket.bind(('0.0.0.0', 0))         # OS-ul alege un port liber automat
+manager_socket.setblocking(False)           # asigura simultanietatea procesarii
 
-    while True:
-        data = client_socket.recv(1024)
-        if not data or data .decode('utf-8') == 'END':
-            break
-        print("Received:", data.decode('utf-8'))
+#afisarea portului local al managerului
+print(f"Managerul ascultă pe portul local: {manager_socket.getsockname()[1]}") 
+
+#cererile catre agenti
+requests = {
+    AGENT_1_ADDR: b"GET /sysDescr/ Agent 1",        #b - reprezinta un sir de bytes
+    AGENT_2_ADDR: b"GET /sysUpTime/ Agent 2"
+}
+
+#transmiterea cererilor catre agenti
+for addr, payload in requests.items():
+    print(f"\n[SEND] Trimit cererea catre {addr}: {payload.decode()}")
+    manager_socket.sendto(payload, addr)
+
+#asteptarea raspunsurilor cu select
+timeout = 5.0 # Asteapta raspunsuri timp de 5 secunde
+responses_received = 0
+expected_responses = len(requests)
+
+print(f"\n[SELECT] Aștept răspunsuri de la cei {expected_responses} Agenți (timeout: {timeout}s)...")
+
+try:
+    #cat timp nu am primit toate raspunsurile asteptate
+    while responses_received < expected_responses:
         
-        try:
-            client_socket.send(bytes("Hey client", 'utf-8'))
-        except:
-            print("Exit by user")
+
+        #selecteaza socket-urile pregatite pentru citire
+        ready_to_read, _, _ = select.select([manager_socket], [], [], timeout)      # _ - variabile neutilizate
+        
+        #daca socket-ul managerului este pregatit pentru citire
+        if manager_socket in ready_to_read:
+
+            #primeste datele si adresa sursa
+            data, addr = manager_socket.recvfrom(1024)
+
+            #afisarea raspunsului primit
+            print(f"--- [RECV] Răspuns primit de la {addr} ---")
+            print(f"Răspuns: {data.decode()}")
+
+            #contorizarea raspunsurilor primite
+            responses_received += 1
+
+        else:
+            #daca s-a atins timeout-ul, iesim din bucla
+            print("\nTimeout: Nu s-au primit toate răspunsurile la timp.")
             break
-    client_socket.close()
-server_socket.close()
+
+#exceptia pentru oprirea managerului cu Ctrl+C
+except KeyboardInterrupt:
+    print("\nManagerul oprit de utilizator.")
+
+#inchiderea socket-ului managerului
+manager_socket.close()
+print("Managerul s-a închis.")
